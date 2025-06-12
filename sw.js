6429bed461afbe2878ce368c54e00f950f5d5524
mwanzia <shem.masoo@student.moringaschool.com>
2025-06-12 03:20:29 +0300

    15th updated commit

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
 self.addEventListener("install", e =>{
-    console.log("install!");
+    e.waitUntil(
+        caches.open("static").then(cache =>{
+            return cache.addAll(["./","./styles.css","./images/logoipsum-custom-logo.png"])
+        })
+    )
+});
+
+self.addEventListener("fetch",e => {
+    e.respondWith(
+       caches.match(e.request).then(response => {
+        return response || fetch(e.request);
+       })
+    );
 })
